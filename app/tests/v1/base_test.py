0941b9217663cace0f7dd1e826297490b9c3954a
mimipeshy <perisndanu@gmail.com>
2018-10-23 07:35:51 +0300

    [#61405239] Added tests

---
+++ app/tests/v1/base_test.py
@@ -0,0 +1,26 @@
+import unittest
+import sys  # fix import errors
+import os
+import json
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from app.app import create_app
+from app.api.v1.models.sales import Sale
+
+class BaseTests(unittest.TestCase):
+    """This class represents the base configurations for all tests"""
+
+    def setUp(self):
+        """Define test variables and initialize app"""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+
+        self.add_product= json.dumps({
+            "product_name":"sugar",
+            "quantity":67,
+            "price":300
+        })
+        self.add_sale=json.dumps({
+            'product_id': 1
+        })
+
---
+++ app/tests/v1/test_products.py
@@ -0,0 +1,33 @@
+import json
+import unittest
+
+from app.tests.v1.base_test import BaseTests
+
+
+class ProductTests(BaseTests):
+    """Tests functionality of the orders endpoint"""
+
+    def test_create_product(self):
+        """Test API can create a product"""
+        response = self.client().post('/api/v1/products', data=self.add_product,
+                                      content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_all_products(self):
+        """Tests API can get all products)"""
+        products = {"products": "products"}
+        response = self.client().get('/api/v1/products', data=products,
+                                     content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_product_by_id(self):
+        """Tests API can get one product by using its id"""
+        # products = {"product_hid": "45678"}
+        response = self.client().get('/api/v1/products/1',
+                                      content_type='application/json',
+                                      )
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
